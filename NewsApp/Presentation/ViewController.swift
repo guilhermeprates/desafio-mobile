f422947b6820514acf8e5daf89b78cba47a97cfa
Guilherme Prates <git@guilhermeprates.dev>
2025-10-13 10:52:12 -0300

    chore: init

---
+++ NewsApp/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Guilherme Prates on 13/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+}
+
